d76d7666b1f4537e7bfc723f2cac4f6d0f9ebde8
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-06-14 11:30:26 +0200

    adding static function

--- functions.py
+++ functions.py
@@ -19,6 +19,23 @@ wordSet = WordSetNonStatic()
 wordSet.addText('This is a very interting speech. He is a GOAT!')
 wordSet.printText()
 
+class WordSetStatic:
+    def __init__(self):
+        self.words = set()
 
+    def addText(self, text):
+        text = WordSetStatic.cleanText(text)
+        for word in text.split():
+            self.words.add(word)
+
+    def cleanText(text):
+        return text.replace('!', '').replace('.', '').replace(',', '').replace('/', '').lower()
+    
+    def printText(self):
+        print(self.words)
+    
+wordSetStatic = WordSetStatic()
+wordSetStatic.addText('This is a very interting speech. He is a GOAT!')
+wordSetStatic.printText()
 
 
